fad3f040b497ffdf427f3e5dcb3593aa55cd84b0
WescleyHenrique <wescleyhenrique0@gmail.com>
2025-06-04 19:59:10 +0000

    CRIAÇÃO DAS MATRIZES DE HABILIDADE

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -101,32 +101,18 @@ int main() {
         }
         printf("\n"); // Quebra de linha para a próxima linha do tabuleiro
     }
+
+    // --- CRIAÇÃO DAS MATRIZES DE HABILIDADE ---
+
+    // MATRIZ DE HABILIDADE: CONE
+    int habilidadeCone[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE];
+    for (int i = 0; i < TAMANHO_HABILIDADE; i++) {
+        for (int j = 0; j < TAMANHO_HABILIDADE; j++) {
+            habilidadeCone[i][j] = 0; // Inicializa a matriz do cone com 0s
+        }
+    }
     
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
